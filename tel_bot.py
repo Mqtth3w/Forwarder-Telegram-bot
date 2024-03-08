2323ff8dbb961af093cabfb7ba59b073138c0eee
Mqtth3w <119968260+Mqtth3w@users.noreply.github.com>
2024-03-08 21:20:09 +0100

    Update tel_bot.py

--- tel_bot.py
+++ tel_bot.py
@@ -60,21 +60,4 @@ updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(MessageHandler(filters.text, send))
 updater.start_polling()'''
 
-'''
-/*
-async function handleRequest(request) {
-  if (request.method === "POST") {
-    const payload = await request.json() 
-    // Getting the POST request JSON payload
-    if ('message' in payload) { 
-      // Checking if the payload comes from Telegram
-      const chatId = payload.message.chat.id
-      const text = payload.message.text 
-      const url = `https://api.telegram.org/bot${API_KEY}/sendMessage?chat_id=${chatId}&text=${text}`
-      const data = await fetch(url).then(resp => resp.json()) 
-      // Calling the API endpoint to send a telegram message
-    }
-  }
-  return new Response("OK") // Doesn't really matter
-}*/
-'''
+
