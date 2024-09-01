70d9d35df5dec133b9952348221e29e8d5839d6e
Mqtth3w <mqtth3w@duck.com>
2024-09-01 18:21:07 +0200

    Update tel_bot.js

--- tel_bot.js
+++ tel_bot.js
@@ -39,9 +39,9 @@ async function ForwardMessage(cId, fcId, mId) {
 
 async function handleRequest(request) {
 	const secret_token = request.headers.get("X-Telegram-Bot-Api-Secret-Token");
-    if (secret_token !== SECRET_TOKEN) {
+    	if (secret_token !== SECRET_TOKEN) {
 		return new Response("Authentication Failed.", { status: 403 });
-    }
+    	}
 	if (request.method === "POST") {
 		const payload = await request.json();
 		if ('message' in payload) {
