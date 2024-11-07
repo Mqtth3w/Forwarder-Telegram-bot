fd7f0d74b608c9c7135847b44bb100e7f6ac772a
Mqtth3w <mqtth3w@duck.com>
2024-11-07 13:19:17 +0100

    Add files via upload
    Bug fixes and various improvements

--- tel_bot.js
+++ tel_bot.js
@@ -1,4 +1,4 @@
-
+// @author Mqtth3w https://github.com/Mqtth3w/
 addEventListener("fetch", event => {
 	event.respondWith(handleRequest(event.request));
 });
@@ -9,11 +9,13 @@ let susp_info = "Sorry, the service is temporarily suspended.";
 let custom_susp = "";
 let pinned_usr = ""; 
 const nick = "Mqtth3w"; // Change it with your nickname
+const pc_user = true; // protect_content: If true protects the contents
+const pc_dest = false; // of the sent message from forwarding and saving
 let url = `https://api.telegram.org/bot${API_KEY}/`;
 const user_guide = "https://github.com/Mqtth3w/Forwarder-Telegram-bot/tree/main#user-guide";
 const faq = "https://github.com/Mqtth3w/Forwarder-Telegram-bot/tree/main#faq";
 
-async function SendMessage(url, cId, txt, pc = true, prf) {
+async function SendMessage(cId, txt, pc = true, prf) {
 	let payload = {
 		chat_id: cId,
 		text: txt,
@@ -39,7 +41,7 @@ async function SendMessage(url, cId, txt, pc = true, prf) {
 	}); 
 };
 
-async function ForwardMessage(url, cId, fcId, mId, pc = true) {
+async function ForwardMessage(cId, fcId, mId, pc = true) {
 	await fetch(url + 'forwardMessage', {
 		method: "POST",
 		headers: {
@@ -52,6 +54,79 @@ async function ForwardMessage(url, cId, fcId, mId, pc = true) {
 		  protect_content: pc,
 		}),
 	});
+};
+
+async function SendMedia(msg, dest, chatId, pc = true, pc_d = false) {
+	let method = "";
+	let method2 = "";
+	let methodr = "";
+	let fileId = "";
+	let payload = { chat_id: chatId, protect_content: pc };
+	if (msg.photo) {
+		method = "sendPhoto";
+		method2 = "photo";
+		methodr = "Photo";
+		fileId = msg.photo[msg.photo.length - 1].file_id;
+	}
+	else if (msg.sticker) {
+		method = "sendSticker";
+		method2 = "sticker";
+		methodr = "Sticker";
+		fileId = msg.sticker.file_id;
+	}
+	else if (msg.document) {
+		method = "sendDocument";
+		method2 = "document";
+		methodr = "Document";
+		fileId = msg.document.file_id;
+	}
+	else if (msg.video) {
+		method = "sendVideo";
+		method2 = "video";
+		methodr = "Video";
+		fileId = msg.video.file_id;
+	} 
+	else if (msg.animation) {
+		method = "sendAnimation";
+		method2 = "animation";
+		methodr = "Animation";
+		fileId = msg.animation.file_id;
+	}
+	else if (msg.audio) {
+		method = "sendAudio";
+		method2 = "audio";
+		methodr = "Audio";
+		fileId = msg.audio.file_id;
+	}
+	else if (msg.voice) {
+		method = "sendVoice";
+		method2 = "voice";
+		methodr = "Voice";
+		fileId = msg.voice.file_id;
+	}
+	else if (msg.location) {
+		method = "sendLocation";
+		methodr = "Location";
+		payload.latitude = msg.location.latitude;
+		payload.longitude = msg.location.longitude;
+	}
+	else {
+		await SendMessage(url, dest, `Unexpected data, reply not sent.`, pc_d);
+		return;
+	}
+
+	if (!msg.location) {
+		payload[method2] = fileId;
+	}
+
+	await fetch(url + `${method}`, {
+		method: "POST",
+		headers: {
+		"Content-Type": "application/json",
+		},
+		body: JSON.stringify(payload),
+	});
+	await SendMessage(url, dest, `${methodr} sent to ${chatId}.`, pc_d, chatId);
 };
 
 async function handleRequest(request) {
@@ -73,30 +148,33 @@ async function handleRequest(request) {
 					const senderId = infoArr[0];
 					if (senderId && Number(senderId) > 0) {
 						if (text) {
-							await SendMessage(url, senderId, text);
-							await SendMessage(url, env.DESTINATION, `Reply sent to ${senderId}.`, false, senderId);
+							await SendMessage(senderId, text, pc_user);
+							await SendMessage(DESTINATION, `Reply sent to ${senderId}.`, pc_dest, senderId);
+						}
+						else {
+							await SendMedia(payload.message, DESTINATION, pinned_usr, pc_user, pc_dest);
 						}
 					}
 					else {
-						await SendMessage(url, env.DESTINATION, "Reply only to messages starting with an user ID.", false);
+						await SendMessage(DESTINATION, "Reply only to messages starting with an user ID.", pc_dest);
 					}
 				}
 				else if (command === "/start") {
-					await SendMessage(DESTINATION, "Hello, chief!", false);
+					await SendMessage(DESTINATION, "Hello, chief!", pc_dest);
 				}
 				else if (command === "/block") {
 					let infoBlock = text.split(" ");
 					if (infoBlock[1] && Number(infoBlock[1]) > 0) {
 						if (!blocked.includes(infoBlock[1])) { // Not already blocked
 							blocked.push(infoBlock[1]);
-							await SendMessage(DESTINATION, `User ${infoBlock[1]} blocked.`, false, infoBlock[1]);
+							await SendMessage(DESTINATION, `User ${infoBlock[1]} blocked.`, pc_dest, infoBlock[1]);
 						}
 						else {
-							await SendMessage(DESTINATION, "User already blocked.", false);
+							await SendMessage(DESTINATION, "User already blocked.", pc_dest);
 						}
 					}
 					else {
-						await SendMessage(DESTINATION, "Invalid User ID.", false);
+						await SendMessage(DESTINATION, "Invalid User ID.", pc_dest);
 					}
 				}
 				else if(command === "/unblock") {
@@ -105,14 +183,14 @@ async function handleRequest(request) {
 						let index = blocked.indexOf(infoBlock[1]);
 						if (index > -1) { // Only splice array when item is found
 							blocked.splice(index, 1); // 2nd parameter means remove one item only
-							await SendMessage(DESTINATION, `User ${infoBlock[1]} unblocked.`, false, infoBlock[1]);
+							await SendMessage(DESTINATION, `User ${infoBlock[1]} unblocked.`, pc_dest, infoBlock[1]);
 						}
 						else {
-							await SendMessage(DESTINATION, "Hey chief, the user is not blocked.", false);
+							await SendMessage(DESTINATION, "Hey chief, the user is not blocked.", pc_dest);
 						}
 					}
 					else {
-						await SendMessage(DESTINATION, "Invalid User ID.", false);
+						await SendMessage(DESTINATION, "Invalid User ID.", pc_dest);
 					}
 				}
 				else if (command === "/suspend") {
@@ -124,15 +202,15 @@ async function handleRequest(request) {
 						custom_susp = "";
 					}
 					suspended = true;
-					await SendMessage(DESTINATION, "Service suspended.", false);
+					await SendMessage(DESTINATION, "Service suspended.", pc_dest);
 				}
 				else if (command === "/unsuspend") {
 					suspended = false;
 					custom_susp = "";
-					await SendMessage(DESTINATION, "Service unsuspended.", false);
+					await SendMessage(DESTINATION, "Service unsuspended.", pc_dest);
 				}
 				else if (command === "/help") {
-					await SendMessage(DESTINATION, `User guide: ${user_guide}. FAQ: ${faq}.`, false);
+					await SendMessage(DESTINATION, `User guide: ${user_guide}. FAQ: ${faq}.`, pc_dest);
 				}
 				else if (command === "/blocked") {
 					let blocked_str = 'blocked = ["' + blocked.join('", "') + '"]';
@@ -142,36 +220,39 @@ async function handleRequest(request) {
 					let infoBlock = text.split(" ");
 					if (infoBlock[1] && Number(infoBlock[1]) > 0) {
 						pinned_usr = infoBlock[1];
-						await SendMessage(DESTINATION, `User ${pinned_usr} pinned.`, false, pinned_usr);
+						await SendMessage(DESTINATION, `User ${pinned_usr} pinned.`, pc_dest, pinned_usr);
 					}
 					else {
-						await SendMessage(DESTINATION, "Invalid User ID.", false);
+						await SendMessage(DESTINATION, "Invalid User ID.", pc_dest);
 					}
 				}
 				else if (command === "/unpin") {
-					await SendMessage(DESTINATION, `User ${pinned_usr} unpinned.`, false, pinned_usr);
+					await SendMessage(DESTINATION, `User ${pinned_usr} unpinned.`, pc_dest, pinned_usr);
 					pinned_usr = "";
 				}
 				else if (command === "/show") {
 					let infoBlock = text.split(" ");
 					if (infoBlock[1] && Number(infoBlock[1]) > 0) {
-						await SendMessage(DESTINATION, `User ${infoBlock[1]}.`, false, infoBlock[1]);
+						await SendMessage(DESTINATION, `User ${infoBlock[1]}.`, pc_dest, infoBlock[1]);
 					}
 					else {
-						await SendMessage(DESTINATION, "Invalid User ID.", false);
+						await SendMessage(DESTINATION, "Invalid User ID.", pc_dest);
 					}
 				}
 				else if (payload.message.entities && payload.message.entities.length > 0 && payload.message.entities[0].type === "bot_command") { 
-					await SendMessage(DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, false);
+					await SendMessage(DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, pc_dest);
 				}
 				else if (pinned_usr) {
 					if (text) {
-						await SendMessage(pinned_usr, text);
-						await SendMessage(DESTINATION, `Reply sent to ${pinned_usr}`, false, pinned_usr);
+						await SendMessage(pinned_usr, text, pc_user);
+						await SendMessage(DESTINATION, `Reply sent to ${pinned_usr}`, pc_dest, pinned_usr);
+					}
+					else {
+						await SendMedia(payload.message, DESTINATION, pinned_usr, pc_user, pc_dest);
 					}
 				}
 				else { 
-					await SendMessage(DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, false);
+					await SendMessage(DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, pc_dest);
 				} 
 			}
 			else if (!blocked.includes(chatId) && suspended) {
@@ -188,17 +269,17 @@ async function handleRequest(request) {
 				let info = chatId + "  " + user;
 				let extraInfo = `language_code:${payload.message.from.language_code} is_bot:${payload.message.from.is_bot}`;
 				if (text === "/start") {
-					await SendMessage(chatId, `Hello, ${user}!`);
-					await SendMessage(DESTINATION, username ? `${info} @${username} ${extraInfo} started the bot.`	: `${info} ${extraInfo} started the bot.`, false, chatId);
+					await SendMessage(chatId, `Hello, ${user}!`, pc_user);
+					await SendMessage(DESTINATION, username ? `${info} @${username} ${extraInfo} started the bot.`	: `${info} ${extraInfo} started the bot.`, pc_dest, chatId);
 				}
 				else if (text === "/help") {
-					await SendMessage(chatId, `This bot forward all messages you send to ${nick}. Through this bot, ${nick} can reply you.`);
-					await SendMessage(DESTINATION, username ? `${info} @${username} ${extraInfo} typed /help.` : `${info} ${extraInfo} typed /help.`, false, chatId);
+					await SendMessage(chatId, `This bot forward all messages you send to ${nick}. Through this bot, ${nick} can reply you.`, pc_user);
+					await SendMessage(DESTINATION, username ? `${info} @${username} ${extraInfo} typed /help.` : `${info} ${extraInfo} typed /help.`, pc_dest, chatId);
 				}
 				else {
-					await SendMessage(chatId, "Message sent.");
-					await ForwardMessage(DESTINATION, chatId, payload.message.message_id);
-					await SendMessage(DESTINATION, username ? `${info} @${username} ${extraInfo}.` : `${info}.`, false, chatId);
+					await SendMessage(chatId, "Message sent.", pc_user);
+					await ForwardMessage(DESTINATION, chatId, payload.message.message_id, pc_dest);
+					await SendMessage(DESTINATION, username ? `${info} @${username} ${extraInfo}.` : `${info}.`, pc_dest, chatId);
 				}
 			}
 		}
--- tel_bot_db.js
+++ tel_bot_db.js
@@ -1,8 +1,12 @@
+// @author Mqtth3w https://github.com/Mqtth3w/
+
 let suspended = false;
 let susp_info = "Sorry, the service is temporarily suspended.";
 let custom_susp = "";
 let pinned_usr = "";
 const nick = "Mqtth3w"; // Change it with your nickname
+const pc_user = true; // protect_content: If true protects the contents
+const pc_dest = false; // of the sent message from forwarding and saving
 const user_guide = "https://github.com/Mqtth3w/Forwarder-Telegram-bot/tree/main#user-guide";
 const faq = "https://github.com/Mqtth3w/Forwarder-Telegram-bot/tree/main#faq";
 
@@ -49,7 +53,7 @@ async function ForwardMessage(url, cId, fcId, mId, pc = true) {
 
 async function SendFormatMessage(url, dest, res, pc = true) {
 	if (res.length === 0) {
-		await SendMessage(url, dest, "No users.", false);
+		await SendMessage(url, dest, "No users.", pc_dest);
 		return;
 	}
 	let total = 0;
@@ -71,7 +75,7 @@ async function SendFormatMessage(url, dest, res, pc = true) {
 			if (i === res.length - 1) {
 				message += `total: ${total}.`;
 			}
-			await SendMessage(url, dest, message, false);
+			await SendMessage(url, dest, message, pc_dest);
 			message = ""; 
 		}
     }
@@ -86,7 +90,80 @@ async function sendBroadcastMessage(url, dest, msg, users, pc = true) {
             await SendMessage(url, dest, `An error occurred while broadcasting to the user ${userId}: ${err}.`, pc, userId);
         }
     }
-}
+};
+
+async function SendMedia(url, msg, dest, chatId, pc = true, pc_d = false) {
+	let method = "";
+	let method2 = "";
+	let methodr = "";
+	let fileId = "";
+	let payload = { chat_id: chatId, protect_content: pc };
+	if (msg.photo) {
+		method = "sendPhoto";
+		method2 = "photo";
+		methodr = "Photo";
+		fileId = msg.photo[msg.photo.length - 1].file_id;
+	}
+	else if (msg.sticker) {
+		method = "sendSticker";
+		method2 = "sticker";
+		methodr = "Sticker";
+		fileId = msg.sticker.file_id;
+	}
+	else if (msg.document) {
+		method = "sendDocument";
+		method2 = "document";
+		methodr = "Document";
+		fileId = msg.document.file_id;
+	}
+	else if (msg.video) {
+		method = "sendVideo";
+		method2 = "video";
+		methodr = "Video";
+		fileId = msg.video.file_id;
+	} 
+	else if (msg.animation) {
+		method = "sendAnimation";
+		method2 = "animation";
+		methodr = "Animation";
+		fileId = msg.animation.file_id;
+	}
+	else if (msg.audio) {
+		method = "sendAudio";
+		method2 = "audio";
+		methodr = "Audio";
+		fileId = msg.audio.file_id;
+	}
+	else if (msg.voice) {
+		method = "sendVoice";
+		method2 = "voice";
+		methodr = "Voice";
+		fileId = msg.voice.file_id;
+	}
+	else if (msg.location) {
+		method = "sendLocation";
+		methodr = "Location";
+		payload.latitude = msg.location.latitude;
+		payload.longitude = msg.location.longitude;
+	}
+	else {
+		await SendMessage(url, dest, `Unexpected data, reply not sent.`, pc_d);
+		return;
+	}
+
+	if (!msg.location) {
+		payload[method2] = fileId;
+	}
+
+	await fetch(url + `${method}`, {
+		method: "POST",
+		headers: {
+		"Content-Type": "application/json",
+		},
+		body: JSON.stringify(payload),
+	});
+	await SendMessage(url, dest, `${methodr} sent to ${chatId}.`, pc_d, chatId);
+};
 
 export default {
 	async fetch(request, env) {
@@ -109,35 +186,38 @@ export default {
 						const senderId = infoArr[0];
 						if (senderId && Number(senderId) > 0) {
 							if (text) {
-								await SendMessage(url, senderId, text);
-								await SendMessage(url, env.DESTINATION, `Reply sent to ${senderId}.`, false, senderId);
+								await SendMessage(url, senderId, text, pc_user);
+								await SendMessage(url, env.DESTINATION, `Reply sent to ${pinned_usr}.`, pc_dest, senderId);
+							}
+							else {
+								await SendMedia(url, payload.message, env.DESTINATION, senderId, pc_user, pc_dest);
 							}
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "Reply only to messages starting with an user ID.", false);
+							await SendMessage(url, env.DESTINATION, "Reply only to messages starting with an user ID.", pc_dest);
 						}
 					}
 					else if (command === "/start") {
-						await SendMessage(url, env.DESTINATION, "Hello, chief!", false);
+						await SendMessage(url, env.DESTINATION, "Hello, chief!", pc_dest);
 					}
 					else if (command === "/block") {
 						let infoBlock = text.split(" "); // Check if they are already blocked may be expensive
 						if (infoBlock[1] && Number(infoBlock[1]) > 0) {
 							await env.db.prepare("UPDATE users SET isblocked = ? WHERE id = ?").bind("true", infoBlock[1]).run();
-							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]} blocked.`, false, infoBlock[1]);
+							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]} blocked.`, pc_dest, infoBlock[1]);
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "Invalid User ID.", false);
+							await SendMessage(url, env.DESTINATION, "Invalid User ID.", pc_dest);
 						}
 					}
 					else if(command === "/unblock") {
 						let infoBlock = text.split(" "); // Check if they are already unblocked may be expensive
 						if (infoBlock[1] && Number(infoBlock[1]) > 0) {
 							await env.db.prepare("UPDATE users SET isblocked = ? WHERE id = ?").bind("false", infoBlock[1]).run();
-							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]} unblocked.`, false, infoBlock[1]);
+							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]} unblocked.`, pc_dest, infoBlock[1]);
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "Invalid User ID.", false);
+							await SendMessage(url, env.DESTINATION, "Invalid User ID.", pc_dest);
 						}
 					}
 					else if (command === "/suspend") {
@@ -149,90 +229,89 @@ export default {
 							custom_susp = "";
 						}
 						suspended = true;
-						await SendMessage(url, env.DESTINATION, "Service suspended.", false);
+						await SendMessage(url, env.DESTINATION, "Service suspended.", pc_dest);
 					}
 					else if (command === "/unsuspend") {
 						suspended = false;
 						custom_susp = "";
-						await SendMessage(url, env.DESTINATION, "Service unsuspended.", false);
+						await SendMessage(url, env.DESTINATION, "Service unsuspended.", pc_dest);
 					}
 					else if (command === "/help") {
-						await SendMessage(url, env.DESTINATION, `User guide: ${user_guide}. FAQ: ${faq}.`, false);
+						await SendMessage(url, env.DESTINATION, `User guide: ${user_guide}. FAQ: ${faq}.`, pc_dest);
 					}
 					else if (command === "/blocked") {
 						const { results } = await env.db.prepare("SELECT * FROM users WHERE isblocked = ?")
 							.bind("true").all();
-						await SendFormatMessage(url, env.DESTINATION, results, false);
+						await SendFormatMessage(url, env.DESTINATION, results, pc_dest);
 					}
 					else if (command === "/pin") {
 						let infoBlock = text.split(" ");
 						if (infoBlock[1] && Number(infoBlock[1]) > 0) {
 							pinned_usr = infoBlock[1];
-							await SendMessage(url, env.DESTINATION, `User ${pinned_usr} pinned.`, false, pinned_usr);
+							await SendMessage(url, env.DESTINATION, `User ${pinned_usr} pinned.`, pc_dest, pinned_usr);
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "Invalid User ID.", false);
+							await SendMessage(url, env.DESTINATION, "Invalid User ID.", pc_dest);
 						}
 					}
 					else if (command === "/unpin") {
-						await SendMessage(url, env.DESTINATION, `User ${pinned_usr} unpinned.`, false, pinned_usr);
+						await SendMessage(url, env.DESTINATION, `User ${pinned_usr} unpinned.`, pc_dest, pinned_usr);
 						pinned_usr = "";
 					}
 					else if (command === "/show") {
 						let infoBlock = text.split(" ");
 						if (infoBlock[1] && Number(infoBlock[1]) > 0) {
-							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]}.`, false, infoBlock[1]);
+							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]}.`, pc_dest, infoBlock[1]);
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "Invalid User ID.", false);
+							await SendMessage(url, env.DESTINATION, "Invalid User ID.", pc_dest);
 						}
 					}
 					else if (command === "/history") {
 						const { results } = await env.db.prepare("SELECT * FROM users").all();
-						await SendFormatMessage(url, env.DESTINATION, results, false);
+						await SendFormatMessage(url, env.DESTINATION, results, pc_dest);
 					}
 					else if (command === "/delete") {
 						let infoBlock = text.split(" ");
 						if (infoBlock[1] && Number(infoBlock[1]) > 0) {
 							await env.db.prepare("DELETE FROM users WHERE id = ?").bind(infoBlock[1]).run();
-							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]} deleted (if exist).`, false, infoBlock[1]);
+							await SendMessage(url, env.DESTINATION, `User ${infoBlock[1]} deleted (if exist).`, pc_dest, infoBlock[1]);
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "Invalid User ID.", false);
+							await SendMessage(url, env.DESTINATION, "Invalid User ID.", pc_dest);
 						}
 					}
 					else if (command === "/broadcast") {
 						let firstSpaceIndex = text.indexOf(' ');
 						if (firstSpaceIndex !== -1) {
 							let msg = text.slice(firstSpaceIndex + 1);
-							try {
-								const { results } = await env.db.prepare("SELECT id FROM users").all();
-								if (results.length > 0) {
-									await sendBroadcastMessage(url, env.DESTINATION, msg, results.map(row => row.id));
-									await SendMessage(url, env.DESTINATION, "Message broadcasted.", false);
-								}
-								else {
-									await SendMessage(url, env.DESTINATION, "There aren't users in your DB.", false);
-								}
-							} catch (err) {
-								await SendMessage(url, env.DESTINATION, `Error: ${err}`, false);
+							const { results } = await env.db.prepare("SELECT id FROM users").all();
+							if (results.length > 0) {
+								await sendBroadcastMessage(url, env.DESTINATION, msg, results.map(row => row.id));
+								await SendMessage(url, env.DESTINATION, "Message broadcasted.", pc_dest);
+							}
+							else {
+								await SendMessage(url, env.DESTINATION, "There aren't users in your DB.", pc_dest);
 							}
 						}
 						else {
-							await SendMessage(url, env.DESTINATION, "You must provide a message to be broadcasted.", false);
+							await SendMessage(url, env.DESTINATION, "You must provide a message to be broadcasted.", pc_dest);
 						}
 					}
 					else if (payload.message.entities && payload.message.entities.length > 0 && payload.message.entities[0].type === "bot_command") { 
-						await SendMessage(url, env.DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, false);
+						await SendMessage(url, env.DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, pc_dest);
 					}
 					else if (pinned_usr) {
 						if (text) {
-							await SendMessage(url, pinned_usr, text);
-							await SendMessage(url, env.DESTINATION, `Reply sent to ${pinned_usr}.`, false, pinned_usr);
+							await SendMessage(url, pinned_usr, text, pc_user);
+							await SendMessage(url, env.DESTINATION, `Reply sent to ${pinned_usr}.`, pc_dest, pinned_usr);
+						}
+						else {
+							await SendMedia(url, payload.message, env.DESTINATION, pinned_usr, pc_user, pc_dest);
 						}
 					}
 					else {
-						await SendMessage(url, env.DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, false);
+						await SendMessage(url, env.DESTINATION, `Hey chief! Invalid command, check the User guide at ${user_guide}.`, pc_dest);
 					}
 				}
 				else { 
@@ -243,45 +322,41 @@ export default {
 						isBlocked = results[0].isblocked;
 					}
 					if (isBlocked === "false" && suspended) {
-						await SendMessage(url, chatId, `${susp_info} ${custom_susp}`);
+						await SendMessage(url, chatId, `${susp_info} ${custom_susp}`, pc_user);
 					}
 					else if (isBlocked === "false") {
 						const first_name = payload.message.from.first_name;
 						const last_name = payload.message.from.last_name;
 						const username = payload.message.from.username;
 						let user = first_name;
-						if (last_name) {
+						if (last_name) { 
 							user = user + " " + last_name;
 						}
 						let info = chatId + "  " + user;
 						const lang = payload.message.from.language_code;
-						const is_bot = payload.message.from.is_bot;
+						const is_bot = payload.message.from.is_bot.toString();
 						let extraInfo = `language_code:${lang} is_bot:${is_bot}`;
 						if (text === "/start") {
 							const { results } = await env.db.prepare("SELECT * FROM users WHERE id = ?")
 								.bind(chatId).all();
 							if (results.length === 0) {
-								try {
-									await env.db.prepare("INSERT INTO users (id, name, surname, username, start_date, isblocked, language_code, is_bot) VALUES (?,?,?,?,?,?,?,?)")
-										.bind(chatId, first_name, last_name || "", username || "", (new Date()).toISOString(), "false", lang, is_bot).run();
-									await SendMessage(url, chatId, `Hello, ${user}!`);
-								} catch (err) {
-									await SendMessage(url, env.DESTINATION, `Error during user ${chatId} start: ${err}.`, false, chatId);
-								}
+								await env.db.prepare("INSERT INTO users (id, name, surname, username, start_date, isblocked, language_code, is_bot) VALUES (?,?,?,?,?,?,?,?)")
+									.bind(chatId, first_name, last_name || "", username || "", (new Date()).toISOString(), "false", lang, is_bot).run();
+								await SendMessage(url, chatId, `Hello, ${user}!`, pc_user);
 							}
 							else {
-								await SendMessage(url, chatId, `Welcome back, ${user}!`);
+								await SendMessage(url, chatId, `Welcome back, ${user}!`, pc_user);
 							}
-							await SendMessage(url, env.DESTINATION, username ? `${info} @${username} ${extraInfo} started the bot.`	: `${info} ${extraInfo} started the bot.`, false, chatId);
+							await SendMessage(url, env.DESTINATION, username ? `${info} @${username} ${extraInfo} started the bot.`	: `${info} ${extraInfo} started the bot.`, pc_dest, chatId);
 						}
 						else if (text === "/help") {
-							await SendMessage(url, chatId, `This bot forward all messages you send to ${nick}. Through this bot, ${nick} can reply you.`);
-							await SendMessage(url, env.DESTINATION, username ? `${info} @${username} ${extraInfo} typed /help.` : `${info} ${extraInfo} typed /help.`, false, chatId);
+							await SendMessage(url, chatId, `This bot forward all messages you send to ${nick}. Through this bot, ${nick} can reply you.`, pc_user);
+							await SendMessage(url, env.DESTINATION, username ? `${info} @${username} ${extraInfo} typed /help.` : `${info} ${extraInfo} typed /help.`, pc_dest, chatId);
 						}
 						else {
-							await SendMessage(url, chatId, "Message sent.");
-							await ForwardMessage(url, env.DESTINATION, chatId, payload.message.message_id, false);
-							await SendMessage(url, env.DESTINATION, username ? `${info} @${username} ${extraInfo}.` : `${info} ${extraInfo}.`, false, chatId);
+							await SendMessage(url, chatId, "Message sent.", pc_user);
+							await ForwardMessage(url, env.DESTINATION, chatId, payload.message.message_id, pc_dest);
+							await SendMessage(url, env.DESTINATION, username ? `${info} @${username} ${extraInfo}.` : `${info} ${extraInfo}.`, pc_dest, chatId);
 						}
 					}
 				}
