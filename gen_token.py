19e37bc0922a855c541592a9e72894effa48f9dd
Matteo Gianvenuti <mqtth3w@duck.com>
2025-07-23 17:41:44 +0200

    Add files via upload

--- gen_token.py
+++ gen_token.py
@@ -7,15 +7,13 @@ import random
 import string
 
 def generate_token(length=256):
-    # Define the allowed characters: A-Z, a-z, 0-9, _, and -
+    # A-Z, a-z, 0-9, _, and -
     characters = string.ascii_letters + string.digits + "_-"
-    # Ensure the token length is within the allowed range (1-256)
     if not (1 <= length <= 256):
-        raise ValueError("Token length must be between 1 and 256 characters.")
-    # Generate a random token using the allowed characters
+        raise ValueError("Token length must be between 1 and 256 characters")
     token = ''.join(random.choice(characters) for _ in range(length))
     return token
 
-# Generate a X-character token
-secret_token = generate_token(32)
-print("Generated Token:", secret_token)
+if __name__ == "__main__":
+    secret_token = generate_token(32)
+    print("Generated Token:", secret_token)
